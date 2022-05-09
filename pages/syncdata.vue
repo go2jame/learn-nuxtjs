eb45cd3d97035432a82f8588734878d8f02ec796
Thananan Chaisiriphutthichok <go2jame@gmail.com>
2022-05-09 17:37:06 +0700

    update

---
+++ pages/syncdata.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!-- <img src="~/assets/logo.svg" /> -->
+    <h1>Data fetched using asyncData</h1>
+    <ul>
+      <li v-for="mountain in mountains" :key="mountain.title">
+        <NuxtLink
+          :to="{ name: 'mountains-slug', params: { slug: mountain.slug } }"
+        >
+          {{ mountain.title }}
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  async asyncData() {
+    const mountains = await fetch('https://api.nuxtjs.dev/mountains')
+      .then((res) => res.json())
+    return { mountains }
+  }
+}
+</script>
